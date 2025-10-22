b9e1238dd14c9d1e8574d9f164a4beba3e29b0b3
BlooberryMoth <namoyedlaz@gmail.com>
2025-10-22 12:58:51 -0600

    Initial Commit

---
+++ HTTPHandler.py
@@ -0,0 +1,25 @@
+import json
+from Main import Server
+
+
+def handle_GET_request(url: list[str], parameters: dict) -> tuple[int, list[tuple[str, str]], str]:
+    name, file_format, *_ = url[-1].split('.') + [""]
+    if file_format:
+        try:
+            with open(f'./http/{"/".join(url)}') as file_in: file = file_in.buffer.read()
+            return 200, [], file
+        except: return Server.fourohfour
+    else:
+        try:
+            with open(f'./http/{"/".join(url)}/index.html') as file_in: index = file_in.buffer.read()
+            return 200, [("Content-Type", "text/html")], index
+        except: return Server.fourohfour
+    
+
+def handle_POST_request(data: bytes) -> tuple[int, list[tuple[str, str]], str]:
+    try: data = json.loads(data.decode())
+    except: return error(400, "Invalid body form.")
+    try: method = data['method']
+    except: return error(400, "No method provided.")
+
+def error(status: int, reason: str): return status, [("Content-Type", "application/json")], json.dumps({"error": status, "reason": reason})
---
+++ Main.py
@@ -0,0 +1,48 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from Logging import LOG
+import HTTPHandler
+
+PORT = 3001
+
+LOG.name = "Image Restaurant"
+
+
+class Server(BaseHTTPRequestHandler):
+    with open('./http/404/index.html') as file_in: index = file_in.buffer.read()
+    fourohfour = 404, [], index
+
+    def do_GET(self) -> None:
+        url, pairs = self.path.removeprefix('/').removesuffix('/').split('?') + [""]
+        parameters = {}
+        for pair in pairs.split('&'):
+            pair = pair.split('=') + [True]
+            parameters[pair[0]] = pair[1]
+
+        status, headers, data = HTTPHandler.handle_GET_request(url.lower().split('/'), parameters)
+        self.send_data(status, headers, data)
+
+
+    def do_POST(self) -> None:
+        body = self.rfile.read(int(self.headers['Content-Length']))
+
+        status, headers, data = HTTPHandler.handle_POST_request(body)
+        self.send_data(status, headers, data)
+
+
+    def send_data(self, status, headers, data) -> None:
+        self.send_response(status)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        for header in headers: self.send_header(*header)
+        self.end_headers()
+        if not isinstance(data, (bytes, bytearray)): data = bytes(data, 'utf-8')
+        self.wfile.write(data)
+
+    def log_message(self, format, *args): pass
+
+
+if __name__ == '__main__':
+    LOG.info("Opening HTTP Server and RESTaurant.")
+    server = HTTPServer(("0.0.0.0", PORT), Server)
+    try: server.serve_forever()
+    except Exception as e: print(e)
+    LOG.warning("Closed HTTP Server and RESTaurant.")
